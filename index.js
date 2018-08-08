0508e4952f3a65f5d480d9999b633a5da5164816
Aaron Yarborough <personal.ajy@gmail.com>
2018-08-08 16:45:43 +0100

    Ditch ignored files

--- index.js
+++ index.js
@@ -3,38 +3,43 @@ const Client = require('instagram-private-api').V1;
 const device = new Client.Device(config.username);
 const storage = new Client.CookieFileStorage(__dirname + '/cookies/cookies.json');
 
-const log4js = require('log4js');
-log4js.configure({
-    appenders: { tracelog: { type: 'file', filename: 'tracelog.log' } },
-    categories: { default: { appenders: ['tracelog'], level: 'all' } }
-});
-  
-const logger = log4js.getLogger('tracelog');
+var loggedIn = false;
+var feed;
+var session;
 
 Client.Session.create(device, storage, config.username, config.password)
-    .then((session) => {
-        return [session, Client.Account.searchForUser(session, 'instagram')];
-    })
-    .spread(function(session, account) {
-		return Client.Relationship.create(session, account.id);
-	})
-	.then(function(relationship) {
-        config.tags.forEach(tag => {
-            const feed = new Client.Feed.TaggedMedia(relationship.session, tag, 5);
-
-            feed.get().then(res => {
-                const media = res[Math.floor(Math.random() * res.length)];
-                const comment = generateComment();
-
-                Client.Comment.create(relationship.session, media.id, comment);
-                logger.debug(`Successfully left comment on ${media.id}`);
-                logger.debug(media);
-            });
-        });
+    .then((sesh) => {
+        console.log('Logged in...');
+        loggedIn = true;
+        init(sesh);
     });
-    
+
+function init(session) {
+    this.session = session;
+    feed = new Client.Feed.TaggedMedia(session, config.tag, 5);
+
+    setInterval(() => {
+        postRandomComment();
+    }, config.commentInterval);
+
+    // postRandomComment();
+
+    console.log('Running!');
+}
+
 function generateComment() {
     const score = Math.floor(Math.random() * 6); // 0 - 5
     const record = config.phrases[Math.floor(Math.random() * config.phrases.length)];
     return `${record} - ${score}/10`;
+}
+
+function postRandomComment() {
+    console.log('Posting comment...');
+    feed.get().then(res => {
+        const media = res[Math.floor(Math.random() * res.length)];
+        const comment = generateComment();
+
+        Client.Comment.create(this.session, media.id, comment);
+        console.log(`Successfully left comment on ${media.id}`);
+    });
 }
