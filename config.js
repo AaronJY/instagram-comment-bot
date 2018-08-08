c3c13a30ee4dab6a358f95527c3f41d09be02036
Aaron Yarborough <personal.ajy@gmail.com>
2018-08-08 17:21:59 +0100

    Updated phrases

--- config.js
+++ config.js
@@ -3,10 +3,8 @@ module.exports = {
     password: 'tenLVHvZA88_1mtx',
     tag: 'foodporn',
     phrases: [
-        'Is that edible?',
+        'Well done! That looks edible',
         'Would look even better with a sausage roll balanced on top',
-        'I didn\'t know you could eat cow pat?',
-        'Are you in a biology lesson?',
         'You\'ve got a bright future in the food industry! As a customer',
         'It\'s so raw it\'s still running around the field',
         'Disklike the use of human limbs',
