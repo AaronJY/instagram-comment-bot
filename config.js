be44a830c0912ce7b67f1adf7837b8bd5fda9221
Aaron Yarborough <personal.ajy@gmail.com>
2018-08-09 15:11:10 +0100

    Update phrases and login info

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 module.exports = {
-    version: "2.1",
-    username: 'ten.outa.ten',
-    password: '40VgV2VgFwAsYO7RmVHFeFZLgk',
+    version: "2.2",
+    username: 'ten.outa.ten.2',
+    password: 'LVHvZA88',
     tag: 'foodporn',
     phrases: [
         'Would look even better with a sausage roll balanced on top.',
-        // 'You\'ve got a bright future in the food industry! As a customer',
-        // 'It\'s so raw it\'s still running around the field',
         'Disapprove of the use of human limbs. Approve of the use of edible food.',
         'Have you tried adding eggplant? It makes everything better.',
-        'Needs more spinach. Robots love spinach.',
-        'Needs more sausage roll. Robots love sausage roll.',
-        "This could only be improved by adding 3 heaped tablespoons of cinnamon."
+        'Needs more spinach.',
+        "This could only be improved by adding 3 heaped tablespoons of cinnamon.",
+        "Where's the meat?",
+        "That's a fancy salad.",
+        "Add 3 shots of your favourite spirit for hightened flavour.",
+        "Is the chicken hidden underneath, or...",
+        "This meal would go great with a Robbinson's fruit shoot."
     ],
-    commentInterval: 60000
+    commentInterval: 120000
 }
